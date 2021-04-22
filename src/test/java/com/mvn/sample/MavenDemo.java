8b4699773911476d582d17c319c8b026ee2c3f52
rameshrathod2805 <rameshrathodk039@gmail.com>
2021-04-22 12:43:06 +0530

    maven is added to remote level

---
+++ src/test/java/com/mvn/sample/MavenDemo.java
@@ -0,0 +1,14 @@
+package com.mvn.sample;
+
+import org.testng.annotations.Test;
+
+public class MavenDemo {
+	
+	static String name = "hello world";
+	
+	public void demo() {
+		System.out.println(name);
+			
+	}
+	
+}
